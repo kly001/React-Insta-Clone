72dbeb0b829aa13380634c267f4535bed7b722d2
kly001 <karenly1961@gmail.com>
2020-03-15 20:19:01 -0400

    ...

--- instagram-app/src/components/PostContainer/PostsCard.js
+++ instagram-app/src/components/PostContainer/PostsCard.js
@@ -1,17 +1,19 @@
 import React from "react";
 
 const PostsCard = props => {
-    console.log("props from PostsCard:", props)
     const post = props.post;
 
     return (
         <div className="postsCard">
         <li>
+            <p>{post.id}</p>
             <h3>{post.username}</h3>
-            <p>{post.thumbnailUrl}</p>
+            {/* <p>{post.thumbnailUrl}</p>
+            <p>{post.imageUrl}</p> */}
         </li>
         </div>
     )
 }
+
 
 export default PostsCard;
